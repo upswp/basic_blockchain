8c05bec42bedbeeb7119c3c60c99ad4689c6ca1a
Park Sangwoo <tkddnsos@gmail.com>
2021-03-05 08:24:27 +0900

    feat / wallet class

---
+++ src/Part2/Wallet.java
@@ -0,0 +1,72 @@
+package Part2;
+
+import java.security.*;
+import java.security.spec.ECGenParameterSpec;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Wallet {
+    public PrivateKey privateKey;
+    public PublicKey publicKey;
+
+    public HashMap<String,TransactionOutput> UTXO_Wallet = new HashMap<String,TransactionOutput>();
+
+    public Wallet() {
+        generateKeyPair();
+    }
+    // 잔고 확인 기능
+    public float getBalance() {
+        float total = 0;
+        for (Map.Entry<String, TransactionOutput> item: Main.UTXOs.entrySet()){
+            TransactionOutput UTXO = item.getValue();
+            if(UTXO.isMine(publicKey)) {
+                UTXO_Wallet.put(UTXO.id,UTXO);
+                total += UTXO.value ;
+            }
+        }
+        return total;
+    }
+    // 송신기능
+    public Transaction sendFunds(PublicKey _recipient,float value ) {
+        if(getBalance() < value) {
+            System.out.println("Not Enough~~!");
+            return null;
+        }
+        ArrayList<TransactionInput> inputs = new ArrayList<TransactionInput>();
+
+        float total = 0;
+        for (Map.Entry<String, TransactionOutput> item: UTXO_Wallet.entrySet()){
+            TransactionOutput UTXO = item.getValue();
+            total += UTXO.value;
+            inputs.add(new TransactionInput(UTXO.id));
+            if(total > value) break;
+        }
+
+        Transaction newTransaction = new Transaction(publicKey, _recipient , value, inputs);
+        newTransaction.generateSignature(privateKey);
+
+        for(TransactionInput input: inputs){
+            UTXO_Wallet.remove(input.transactionOutputId);
+        }
+
+        return newTransaction;
+    }
+
+    public void generateKeyPair() {
+        try {
+            KeyPairGenerator keyGen = KeyPairGenerator.getInstance("ECDSA","BC");
+            SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
+            ECGenParameterSpec ecSpec = new ECGenParameterSpec("prime192v1");
+
+            keyGen.initialize(ecSpec, random);
+            KeyPair keyPair = keyGen.generateKeyPair();
+
+            privateKey = keyPair.getPrivate();
+            publicKey = keyPair.getPublic();
+
+        }catch(Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
